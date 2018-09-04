f65b802d9f5a5679ccd79334140e75c9ea2e0a81
freckie <freckie@frec.kr>
2018-09-04 16:30:22 +0900

    Fixed: exposed personal info

--- cafe_crawl.py
+++ cafe_crawl.py
@@ -11,7 +11,7 @@ from selenium import webdriver
 
 # 전역변수
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'}
-accounts = {'id': 'rlaaudgu2', 'pw': 'kimh1785*'}
+accounts = {'id': '', 'pw': ''}
 driver_loc = './chromedriver.exe'
 driver = webdriver.Chrome(driver_loc)
 
